8f851fa5bb0813b0b0aea33c4cb073bc2a182f9e
Alexandr Sugak <aleksandr.sugak@gmail.com>
2015-09-27 16:42:30 +0300

    fix index text, remove store connector since not needed

--- Src/Sample.UI/components/IndexView.jsx
+++ Src/Sample.UI/components/IndexView.jsx
@@ -23,20 +23,15 @@ var IndexView = React.createClass({
 				<p>
 					Here, <mark>Documents</mark> demostrate <mark>Pessimistic</mark> concurrency control.
 					Users are required to <mark>check out</mark> (i.e. lock) the document before making any changes to it.
-					If the document is locked by a user, no other user can edit it.
+					If the document is locked by a user, other users can not edit or delete it.
 				</p>
 				<p>
 					<mark>Tickets</mark> demostrate <mark>Optimistic</mark> concurrency control.
 					Users can edit any ticket. However, the changes made to ticket will only apply if no other user has made changes not seen by the first user.
-					If this is not true, user will have to resolve the <mark>Conflict</mark> by choosing which changes (his or server's) to apply.
+					If this is not true, user will have to resolve the <mark>Conflict</mark> by choosing which changes (his or server version) to apply.
 				</p>
 		    </div>
 		);
-	}
-});
-
-IndexView = connectToStores(IndexView, [], function(context, props) {
-	return {
 	}
 });
 
